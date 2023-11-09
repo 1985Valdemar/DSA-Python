d1f41e561778eed5fcc4ee7d9a223c6324b253d2
1985Valdemar <vteider@yahoo.com>
2023-11-09 08:59:53 -0300

    Bora Iniciar Exercicios

---
+++ Capitulo_5/calculadora_v1.py
@@ -0,0 +1,50 @@
+# Calculadora em Python
+
+# Desenvolva uma calculadora em Python com tudo que você aprendeu nos capítulos até aqui no curso.
+# A solução será apresentada no próximo capítulo!
+
+from cmath import sqrt
+
+print("\n******************* Calculadora em Python *******************")
+
+print("Selecione o Número Da Operação Desejada")
+
+print(" 1 - Soma")
+print(" 2 - Subtração")
+print(" 3 - Multiplicação")
+print(" 4 - Divisão")
+print(" 5 - Potencia")
+print(" 6 - Raiz Quadrada")
+
+menuopcao = input("Qual Operação Deseja: ")
+
+numero1 = float(input("Digite um Numero: "))
+if menuopcao == "5" or menuopcao == "6":
+    print("Numero 2 é zero")
+numero2 = float(input("Digite um Numero: "))
+if menuopcao == "1":
+    print("\n")
+    print("Seu Resultado é:", numero1, "+", numero2, "=",  numero1+numero2 )
+    print("\n")
+elif menuopcao == "2":
+    print("\n")
+    print("Seu Resultado é:", numero1, "-", numero2, "=", numero1-numero2)
+    print("\n")
+elif menuopcao == "3":
+    print("\n")
+    print("Seu Resultado é:", numero1, "*", numero2, "=",  numero1*numero2)
+    print("\n")
+elif menuopcao == "4":
+    print("\n")
+    print("Seu Resultado é:", numero1, "/", numero2, "=",  numero1/numero2)
+    print("\n")
+elif menuopcao == "5":
+    print("\n")
+    print("Seu Resultado é:", numero1, "**", numero2, "=",  numero1 ** 2)
+    print("\n")
+elif menuopcao == "6":
+    print("\n")
+    print("Seu Resultado é:", numero1, "√x", numero2, "=",  (sqrt(numero1)))
+    print("\n")
+else:
+    print("Digite Novamente aconteceu Erro")
