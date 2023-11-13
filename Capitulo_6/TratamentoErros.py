5959042658ff1ebc3c6078e7f6a2fa2889f74d1e
1985Valdemar <vteider@yahoo.com>
2023-11-13 16:26:07 -0300

    Exercicios Cap6 ok

---
+++ Capitulo_6/TratamentoErros.py
@@ -0,0 +1,14 @@
+# TRATAMENTO ERROS TENTAR EVITAR ERROS
+def askint():
+    while True:
+        try:
+            val = int(input("Digite um número: "))
+        except:
+            print("Você não digitou um número!")
+            continue
+        else:
+            print("Obrigado por digitar um número!")
+            break
+        finally:
+            print("Fim da execução!")
+        print(val)
