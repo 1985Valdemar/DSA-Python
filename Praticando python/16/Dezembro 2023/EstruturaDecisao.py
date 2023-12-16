47d78a4fdb53cd14cd818be19df02a82d0f5b09a
1985Valdemar <vteider@yahoo.com>
2023-12-16 10:16:31 -0300

    atualização de python

---
+++ Praticando python/16/Dezembro 2023/EstruturaDecisao.py
@@ -0,0 +1,3 @@
+#Estrutura de Decisão:
+#Verifique se um número é par.
+#Escreva um programa que determine se uma palavra fornecida pelo usuário é um palíndromo.
---
+++ Praticando python/16/Dezembro 2023/EstruturaSequencial.py
@@ -0,0 +1,14 @@
+#Estrutura Sequencial:
+#Crie um programa que exiba “Olá, Mundo!” na tela.
+print("Olá Mundo")
+
+#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+print("\n****** Seja Bem Vindo ******")
+nome = input("Digite Seu Nome: ")
+print("Seja Bem Vindo/a",nome,"Estamos Feliz Por Estar Aqui :)")
+
+#Leia dois números e mostre a soma entre eles.
+print("\n###### Seja Bem Vindo á Soma Simples ######")
+numero1 = float(input("Digite Numero: "))
+numero2 = float(input("Digite Numero: "))
+print("Sua soma é = ",numero1, "+", numero2, "=", numero1 + numero2,"\n")
